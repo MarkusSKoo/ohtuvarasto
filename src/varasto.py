6c759ad2eb8948c1197aeb7c33486427fa505396
MarkusSKoo <markus.kauhanen@helsinki.fi>
2025-11-05 16:30:14 +0200

    Try Pylint mistake in workflow

--- src/varasto.py
+++ src/varasto.py
@@ -15,8 +15,7 @@ class Varasto:
             return alku_saldo
         return tilavuus
 
-    # huom: ominaisuus voidaan myös laskea.
-    # Ei tarvita erillistä kenttää viela_tilaa tms.
+    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
